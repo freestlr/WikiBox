f4dcc55a5112618e3570320e44fa8536e8297063
freestlr <freestlr@gmail.com>
2011-06-29 22:38:43 +0300

    now using jquery, improve tooltip object, refactoring

--- s-wikibox.js
+++ s-wikibox.js
@@ -1,32 +1,30 @@
-Object.prototype.each = function(fn) {
-    for(var p in this) this.hasOwnProperty(p) && fn.call(this[p])
-};
-Array.prototype.each = function(fn) {
-    var i=0,l;
-    for(l=this.length;i<l;i++)fn.call(this[i])
-};
-
-
 var WikiBox = (function(){
-    var     //classes section
-    cTable = function(id){
-        this.header = new cRow();
-        this.body = {};
-    }
-    cTable.prototype = {
-        fill: function(data){}        
-    }
+    var
+    tables = [],
+    form = {},
+    data = {},
     
-    
-            //end classes
-    
-    tooltip = (function(){
-        var 
-        node = document.createElement('div');   //TODO: need to describe css
-        node.id = 'tooltip';
-        node.style.display = 'none';
-        document.body.appendChild(node);
+    Tooltip = (function(){
+        var node = $('<div>')       //TODO: need to describe css
+        .attr('id','wb-tooltip')
+        .css('display','none')
+        .appendTo(document.body)
+        .bind('click', handle)[0];
+
+        node.header = $('<div>')
+        .attr('id','wb-tt-header')
+        .appendTo(node)[0];
+        node.status = $('<div>')
+        .attr('id','wb-tt-status')
+        .appendTo(node)[0];
+        node.details = $('<div>')
+        .attr('id','wb-tt-details')
+        .appendTo(node)[0];
+        node.bzlist = $('<div>')
+        .attr('id','wb-tt-bzlist')
+        .appendTo(node)[0];
         
+        content = [],
         
         _render = function(conf) {
             var
@@ -36,37 +34,28 @@ var WikiBox = (function(){
             text = conf.date || '';
             
             
-        }
-        _moveTo = function(elem) {}
+        },
+        _moveTo = function(elem) {},
+        _view = function(elem) {},
+        _edit = function(elem) {};
         
         return {
-            show: function(id, o){
+            show: function(id, el){
+                var col = el.className.replace(/^.*?cell\-([\d]+).*/,'$1');
+                content = data[id][col];
+                content ? _view() : _edit();
                 _render({
                     date:data[id].date,
                     status:data[id].status,
                     links:data[id].links,
                     text:data[id].text
                 })
-                _moveTo(o);
+                _moveTo(el);
+                node.style.display = 'block';
             }
         }
-    })();
-    tables = [],
-    form = {},
-    data = {}
+    })(),
     
-    _getContent = function(){
-        var url = location.href.replace('view','edit')
-            + '?t=' + Math.random().toString().substr(2,10)
-            + ';nowysiwyg=1';
-        var req = new XMLHttpRequest();
-        req.open('get', url, true);
-        req.onreadystatechange = function(){
-            this.readyState^4
-                ||_prepare(this.responseText);
-        };
-        req.send(null);
-    }
     _prepare = function(text){
         text = text
         .replace(/<script>[\w\W]*?<\/script>/gim, '')   //delete all scripts
@@ -78,13 +67,13 @@ var WikiBox = (function(){
         divs = /&lt;div.*?class\="wb-table".*?&gt;/gm,
         newline = '\n',
         scriptText = ''
-            + newline + '<!-- <pre> -->'
-            + newline + '<script id="wb-script" type="text/Javascript">'
-            + newline + '//<!--'
-            + newline + 'WikiBox.set('+JSON.stringify(tables)+');'
-            + newline + '//-->'
-            + newline + '</script>'
-            + newline + '<!-- <pre> -->';
+            + newline + '&lt;!-- &lt;pre&gt; --&gt;'
+            + newline + '&lt;script id="wb-script" type="text/Javascript"&gt;'
+            + newline + '//&lt;!--'
+            + newline + 'WikiBox.set('+JSON.stringify(data)+');'
+            + newline + '//--&gt;'
+            + newline + '&lt;/script&gt;'
+            + newline + '&lt;!-- &lt;pre&gt; --&gt;';
         
         var div = document.createElement('div');
         div.setAttribute('id', 'wb-edit-content');
@@ -92,60 +81,69 @@ var WikiBox = (function(){
         
         form.nowysiwyg = 1;
         form.settingstopic = '%SETTINGSTOPIC%';
-        form.originalrev = document.querySelector('[name="originalrev"]').value;
+        form.originalrev = div.querySelector('[name="originalrev"]').value;
         form.sig = div.querySelector('#sig').innerHTML;
         form.text = div.querySelector('#topic').innerHTML
         .replace(document.querySelector('#wb-script') ? script : /$/, scriptText)
         .replace(divs, function(){return '&lt;div class="wb-table" id="'+tables[i++].id+'"&gt;'})
         
-    }
-    _submit = function(text,callback){
-        var req = new XMLHttpRequest();
-        req.open('post', location.href.replace('view','save'), true);
-        req.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-        req.onreadystatechange = function(){this.readyState^4||callback()}
-        req.send(text)
-    }
+    },
     _render = function(){}
-    _generateIds = function(aTables) {
-        aTables.each(function() {
-            var id;
-            do {id = Math.random().toString().substr(2,10)}
-            while(!tables.ids[id])
-            this.id = id;
+    _save = function(){
+        var content = '', i=0;
+        $(form).each(function(){content+='&'+this+'='+escape(form[this])})
+        content = content.replace(/^&/,'');
+        $.ajax({
+            url: location.href.replace('view','save'),
+            method: 'post',
+            data: content,
+            success: function(){}
         })
-    }
-    _addEvents = function(arr) {
-        arr.each(function(){this.addEventListener('click', function(event){
-            /wb-cell-status/.test(event.target.className)&&tooltip.show(this.id, event.target);
-            /wb-button-save/.test(event.target.className)&&_save();
-        }, false)})
+    },
+    
+    handleTableClick = function(event) {
+        var cls = event.target.className;
+        /wb-cell-status/.test(cls) && Tooltip.show(this.id, event.target);
+        /wb-button-save/.test(cls) && _save();
     }
     
     return {
         init: function(){
-            _getContent();
-            tables = document.getElementsByClassName('wikibox');
-            if(tables[0])return;    //nothing to do
-            tables.raw = [];
-            tables.each(function() {
-                this.id ? (tables.ids[this.id] = this) : tables.raw.push(this)
+            $.ajax({
+                url: location.href.replace('view','edit')
+                    + '?t=' + Math.random().toString().substr(2,10)
+                    + ';nowysiwyg=1',
+                method: 'get',
+                success: _prepare
             });
-            tables.raw.length && _generateIds(tables.raw);
-            delete tables.raw;
-            data.each(function() {
-                tables.ids[this] ? tables.ids[this].fill(data[this])
+            tables = document.querySelector('.wikibox');
+            if(!tables[0])return;           //no tables - nothing to do
+            tables.ids = {};
+            $(tables).each(function() {     //store existing ids and generate new
+                if(!this.id) {
+                    var id;                 //each id is ten-digit number
+                    do { id = Math.random().toString().substr(2,10) }
+                    while(tables.ids[id])
+                    this.id = id;
+                }
+                tables.ids[this.id] = this;
+                $(this).bind('click', handleTableClick)
+            });
+            $(data).each(function() {       //fill tables with stored data
+                tables.ids[this]
+                    ? tables.ids[this].fill(data[this])
                     : data[this].raw()
             });
-            
-            _addEvents(tables);
         },
-        save: function(){
-            var content = '';
-            form.each(function(){content+='&'+this+'='+escape(form[this])})
-            content = content.replace(/^&/,'');
-            _submit(content, fn)
-        },
-        set: function(o){}
+        
+        set: function(o){           //make some verifications here if needed
+            $(o).each(function(){
+                data[this] = o[this]
+            })
+        }
     }
 })();
+
+window.onload = function() {
+    WikiBox.init();
+}
